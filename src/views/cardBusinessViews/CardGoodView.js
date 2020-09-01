c83f97afdd734946558f50b365c601f183c047a6
tanjinping <tjp@prismslight.com>
2020-09-01 09:54:31 +0800

    卡密商品

--- src/views/cardBusinessViews/CardGoodView.js
+++ src/views/cardBusinessViews/CardGoodView.js
@@ -6,34 +6,87 @@ import good2 from '../../icons/good/good2.png'
 import good3 from '../../icons/good/good3.png'
 import good4 from '../../icons/good/good4.png'
 import good6 from '../../icons/good/good6.png'
+import good7 from '../../icons/good/good7.png'
+import good9 from '../../icons/good/good9.png'
+import good10 from '../../icons/good/good10.png'
 
 function CardGoodView () {
+  const [visible, setVisible] = useState(false)
+  const [actionId, setActionId] = useState(2)
+
+  function handleOk (e) {
+    console.log(e);
+    setVisible(false)
+  };
+
+  function handleCancel (e) {
+    console.log(e);
+    setVisible(false)
+  };
+
   return (
-    <div style={styles.container}>
-      <div style={styles.header}>
-        <div style={styles.headerL}>
-          <HeaderItem />
+    <div className="container">
+      <div className="container" style={styles.container}>
+        <div style={styles.header}>
+          <div style={styles.headerL}>
+            <HeaderItem />
+          </div>
+          <Button icon={
+            <img src={good7} alt="" style={{width:10,marginRight:5,marginBottom:1}} />
+          }
+          type = "primary"
+          size = "small"
+          style = { { fontSize: 12 } } > 添加卡密 < /Button>
         </div>
-        <Button type="primary" size="small">添加卡密</Button>
+        <RTable setVisible={setVisible} />
       </div>
-      <RTable />
+      <Modal
+        visible={visible}
+        onOk={handleOk}
+        footer={null}
+        onCancel={handleCancel}
+      >
+        <div style={{
+          display:'flex',
+          flexDirection:'column',
+          alignItems:'center',
+          padding:25,
+          }}>
+          <img src={good6} alt="" style={{width:90}} />
+          <h4 style={{marginBottom:25,marginTop:25}}>{actionId===1?"确定要删除此支付账户吗？":"确定要删除这个分类吗？"}</h4>
+          {(()=>{
+          if(actionId===1){
+            return <p>分类<span style={{color:"#2C68FF"}}>哔哩哔哩</span> 一共包含了 15 个商品，包含商品的分类不允许被删除，请更改关联商品的分类之后重试。</p>
+          }
+            return <p>删除的分类不可被找回，请确认。</p>
+          })()}
+          <div style={{display:'flex',justifyContent:'space-around',marginTop:25,alignItems:'center',width:'100%'}}>
+            <Button key="back" style={{width:150}}>
+              取消
+            </Button>
+            <Button key="submit"style={{width:150}} type="primary" onClick={handleOk}>
+              确定
+            </Button>
+          </div>
+        </div>
+      </Modal>
     </div>
   )
 }
 
-function RTable () {
+function RTable ({ setVisible }) {
   const [selectionType, setSelectionType] = useState('checkbox');
-  const [visible, setVisible] = useState(false)
-  const [actionId, setActionId] = useState(2)
 
   const columns = [
     {
       title: 'id',
       dataIndex: 'name',
+      align: 'center',
   },
     {
       title: '商品名称',
       dataIndex: 'chinese',
+      align: 'center',
       sorter: {
         compare: (a, b) => a.chinese - b.chinese,
         multiple: 3,
@@ -41,6 +94,7 @@ function RTable () {
   },
     {
       title: '商品分类',
+      align: 'center',
       dataIndex: 'math',
       sorter: {
         compare: (a, b) => a.math - b.math,
@@ -50,6 +104,7 @@ function RTable () {
     {
       title: '下单模型',
       dataIndex: 'english',
+      align: 'center',
       sorter: {
         compare: (a, b) => a.english - b.english,
         multiple: 1,
@@ -58,25 +113,31 @@ function RTable () {
     {
       title: '进价',
       dataIndex: 'name',
+      align: 'center',
   },
     {
       title: '售价',
       dataIndex: 'name',
+      align: 'center',
   },
     {
       title: '密价',
+      align: 'center',
       dataIndex: 'name',
   },
     {
       title: '单位',
       dataIndex: 'name',
+      align: 'center',
   },
     {
       title: '下单限制',
+      align: 'center',
       dataIndex: 'name',
   },
     {
       title: '状态',
+      align: 'center',
       dataIndex: 'name',
       render: name => {
         return (
@@ -88,6 +149,7 @@ function RTable () {
   },
     {
       title: '操作',
+      align: 'center',
       key: "action",
       render: (text, record) => (
         <Space size="small">
@@ -100,7 +162,7 @@ function RTable () {
   const data = [
     {
       key: '1',
-      name: 'John Brown',
+      name: 'rown',
       chinese: 98,
       math: 60,
       english: 70,
@@ -219,122 +281,77 @@ function RTable () {
   </Menu>
   );
 
-  function handleOk (e) {
-    console.log(e);
-    setVisible(false)
-  };
-
-  function handleCancel (e) {
-    console.log(e);
-    setVisible(false)
-  };
-
   return (
-    <div style={{
-      height:'100%',
-    }}>
-      <Modal
-        // title="确定要删除此支付账户吗？"
-        visible={visible}
-        onOk={handleOk}
-        footer={null}
-        onCancel={handleCancel}
-      >
-        <div style={{
-          display:'flex',
-          flexDirection:'column',
-          alignItems:'center',
-          padding:25,
-          }}>
-          <img src={good6} alt="" style={{width:90}} />
-          <h4 style={{marginBottom:25,marginTop:25}}>{actionId===1?"确定要删除此支付账户吗？":"确定要删除这个分类吗？"}</h4>
-          {(()=>{
-          if(actionId===1){
-            return <p>分类<span style={{color:"#2C68FF"}}>哔哩哔哩</span> 一共包含了 15 个商品，包含商品的分类不允许被删除，请更改关联商品的分类之后重试。</p>
-          }
-            return <p>删除的分类不可被找回，请确认。</p>
-          })()}
-          <div style={{display:'flex',justifyContent:'space-around',marginTop:25,alignItems:'center',width:'100%'}}>
-            <Button key="back" style={{width:150}}>
-              取消
-            </Button>
-            <Button key="submit"style={{width:150}} type="primary" onClick={handleOk}>
-              确定
-            </Button>
-          </div>
-        </div>
-      </Modal>
-      <div style={styles.main}>
-        <div style={{
-          display:'flex',
-          alignItems:'center',
-          justifyContent:'space-between',
-          height:'12%',
-          flexWrap:'nowrap',
-          borderBottomWidth:1,
-          borderBottomStyle:'solid',
-          borderBottomColor:'#979BA3',
-          marginLeft:'2%',
-          marginRight:'2%',
-          width:'96%',
-        }}>
-          <div>
-            <Input placeholder="请输入商品编号" size="small" style={{width:150}}/>
-            <Dropdown overlay={menu}>
-              <Button size="small" style={styles.dropdown}>
-                请选择商品分类 <DownOutlined />
-              </Button>
-            </Dropdown>
-            <Dropdown overlay={menu} style={styles.dropdown}>
-              <Button style={styles.dropdown} size="small">
-                请选择商品状态 <DownOutlined />
-              </Button>
-            </Dropdown>
-            <Dropdown overlay={menu} style={styles.dropdown}>
-              <Button style={styles.dropdown} size="small">
-                请选择用户权限 <DownOutlined />
-              </Button>
-            </Dropdown>
-            <Dropdown overlay={menu} style={styles.dropdown}>
-              <Button style={styles.dropdown} size="small">
-                请选择供货商 <DownOutlined />
-              </Button>
-            </Dropdown>
-          </div>
-          <div>
-            <Button size="small" style={{color:'#979BA3',marginRight:15}}>重置</Button>
-            <Button size="small" type="primary" icon={<SearchOutlined />}>
-              Search
-            </Button>
-          </div>
-        </div>
-        <div style={{
-          display:'flex',
-          alignItems:'center',
-          height:'12%',
-          flexWrap:'nowrap',
-          paddingLeft:'2%',
-          paddingRight:'2%',
-          width:'100%',
-        }}>
+    <div style={styles.main}>
+      <div style={{
+        display:'flex',
+        alignItems:'center',
+        justifyContent:'space-between',
+        height:'10%',
+        flexWrap:'nowrap',
+        borderBottomWidth:1,
+        borderBottomStyle:'solid',
+        borderBottomColor:'rgba(0, 0, 0, 0.09)',
+        marginLeft:'2%',
+        marginRight:'2%',
+        width:'96%',
+      }}>
+        <div>
+          <Input placeholder="请输入商品编号" size="small" style={{width:120,fontSize:12}}/>
           <Dropdown overlay={menu}>
-            <Button size="small">
-              批量操作 <DownOutlined />
+            <Button size="small" style={styles.dropdown}>
+              请选择商品分类 <DownOutlined />
             </Button>
           </Dropdown>
-          <Button style={{marginLeft:15}} onClick={()=>setVisible(true)} size="small">执行操作</Button>
+          <Dropdown overlay={menu} style={styles.dropdown}>
+            <Button style={styles.dropdown} size="small">
+              请选择商品状态 <DownOutlined />
+            </Button>
+          </Dropdown>
+          <Dropdown overlay={menu} style={styles.dropdown}>
+            <Button style={styles.dropdown} size="small">
+              请选择用户权限 <DownOutlined />
+            </Button>
+          </Dropdown>
+          <Dropdown overlay={menu} style={styles.dropdown}>
+            <Button style={styles.dropdown} size="small">
+              请选择供货商 <DownOutlined />
+            </Button>
+          </Dropdown>
         </div>
-      <Table columns={columns} rowSelection={{
-        type: selectionType,
-        ...rowSelection
-      }} dataSource={data} rowClassName={(record,index)=>{
-        if(record.name==="John Brown") {
-          return "greenRow"
-        }
-        console.log(index)
-        console.log(record)
-        }} size="small" onChange={onChange} />
+        <div>
+          <Button size="small" style={{color:'#979BA3',marginRight:15,fontSize:12,height:20}}>重置</Button>
+          <Button icon={
+            <img src={good9} alt="" style={{width:10,marginRight:5,marginBottom:1}} />
+          }
+          type = "primary"
+          size = "small"
+            style = { { fontSize: 12,height:20 } } > 搜索商品 < /Button>
+        </div>
       </div>
+      <div style={{
+        display:'flex',
+        alignItems:'center',
+        height:'10%',
+        flexWrap:'nowrap',
+        width:'100%',
+      }}>
+        <Dropdown overlay={menu}>
+          <Button size="small" style={{
+            marginLeft: 20,
+            height: 20,
+            fontSize: 12
+          }}>批量操作<DownOutlined />
+          </Button>
+        </Dropdown>
+        <Button style={{marginLeft:20,height:20,fontSize:12}} onClick={()=>setVisible(true)} size="small">执行操作</Button>
+      </div>
+    <Table columns={columns} rowSelection={{
+      type: selectionType,
+      ...rowSelection
+    }} dataSource={data} rowClassName={(record,index)=>{
+      return "f1f5ff"
+      }} size="small" onChange={onChange} />
     </div>
   )
 }
@@ -366,6 +383,12 @@ function HeaderItem () {
       icon: good4,
       id: 444,
     },
+    {
+      label: '库存预警',
+      number: 10100,
+      icon: good10,
+      id: 555,
+    },
   ]
 
   data.forEach((item, index) => {
@@ -375,10 +398,10 @@ function HeaderItem () {
         boxSizing:'border-box',
         display:'flex',
         alignItems:'center',
-        paddingLeft:index?25:0,
+        paddingLeft:index?15:0,
         height:'100%',
         }} key={id}>
-        <img src={icon} alt="" style={{height:'100%'}} />
+        <img src={icon} alt="" style={{height:'80%'}} />
         <div style={{
           boxSizing:'border-box',
           display: 'flex',
@@ -386,9 +409,10 @@ function HeaderItem () {
           alignItems:'flex-start',
           justifyContent:'center',
           paddingRight:35,
-          borderRightColor:'#D6D7DB',
+          borderRightColor:'rgba(214, 215, 219, 0.55)',
           borderRightWidth:1,
-          borderRightStyle:'solid'
+          height:'40%',
+          borderRightStyle:'solid',
         }}>
           <div style={{color:'#000',fontSize:20,fontWeight:800}}>{number}</div>
           <div style={{fontSize:12,color:'#6F717E'}}>{label}</div>
@@ -402,13 +426,15 @@ function HeaderItem () {
 
 const styles = {
   container: {
-    height: '100%',
     display: 'flex',
     flexDirection: 'column',
     justifyContent: 'space-between',
   },
   dropdown: {
-    marginLeft: 15,
+    marginLeft: 20,
+    width: 120,
+    height: 20,
+    fontSize: 12
   },
   main: {
     width: '100%',
@@ -417,7 +443,7 @@ const styles = {
     borderRadius: 2
   },
   header: {
-    height: '12%',
+    height: '10%',
     width: '100%',
     background: '#fff',
     display: 'flex',
@@ -431,7 +457,7 @@ const styles = {
     display: 'flex',
     alignItems: 'center',
     height: '100%',
-    width: '70%',
+    width: '80%',
   }
 }
 
