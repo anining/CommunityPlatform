d933aef78b1f99710b0073379d1b248d73130c9b
tanjinping <tjp@prismslight.com>
2020-09-22 18:42:00 +0800

    登录日志

--- src/views/webSettingViews/LoggerView.js
+++ src/views/webSettingViews/LoggerView.js
@@ -7,7 +7,7 @@ import { loginlogs } from "../../utils/api"
 function LoggerView () {
 
   return (
-    <div className="container">
+    <div className="view">
       <div className={c.container}>
         <RTable/>
       </div>
@@ -20,13 +20,16 @@ function RTable () {
   const [current, setCurrent] = useState(1)
   const [pageSize] = useState(10)
   const [total, setTotal] = useState(0)
+  const [manager_id, setManager_id] = useState()
+  const [date, setDate] = useState([])
+  const [moment, setMoment] = useState()
 
   useEffect(() => {
     get(current)
   }, [])
 
   function get (current) {
-    loginlogs(current, pageSize).then(r => {
+    loginlogs(current, pageSize, manager_id, date[0], date[1]).then(r => {
       if (!r.error) {
         const { data, total } = r
         setTotal(total)
@@ -35,10 +38,20 @@ function RTable () {
     })
   }
 
+  function dateChange (data, dataString) {
+    setDate(dataString)
+    setMoment(data)
+  }
+
+  function reset () {
+    setDate([])
+    setMoment(undefined)
+    setManager_id(undefined)
+  }
+
   function format (arr) {
     arr.forEach((item, index) => {
       item.key = index
-      // item.account = item.manager.account
     })
     return arr
   }
@@ -65,7 +78,7 @@ function RTable () {
       dataIndex: 'text',
       align: 'center',
       render: (text, record, index) => {
-        return 1
+        return ''
         // const { status, t } = text;
         // return (
         //   <div style={{color:obj[status]}}>{t}</div>
@@ -83,29 +96,37 @@ function RTable () {
     <div className={c.main} style={{marginTop:0}}>
       <div className={c.searchView}>
         <div className={c.search} style={{borderBottomWidth:0}}>
-          <div className={c.searchL} style={{width:'36.822%'}}>
-            <Input placeholder="请输入操作人账号" size="small" className={c.searchInput} style={{width:'35.117%'}}/>
-            <DatePicker.RangePicker style={{width:'58.193%',height:32}}/>
-            </div>
-            <div className={c.searchR}>
-              <Button size="small" className={c.resetBtn}>重置</Button>
-              <Button icon={
-                <img src={good9} alt="" style={{width:14,marginRight:6}} />
-              }
+          <div className={c.searchL}>
+            <Input onPressEnter={()=>get(current)} placeholder="请输入操作人账号" onChange={e=>setManager_id(e.target.value)} value={manager_id} size="small" className={c.searchInput}/>
+            <DatePicker.RangePicker
+              format="YYYY-MM-DD"
+              onChange={dateChange}
+              value={moment}
+              className={c.dataPicker}/>
+          </div>
+          <div className={c.searchR}>
+            <Button size="small" onClick={reset} className={c.resetBtn}>重置</Button>
+            <Button icon={
+              <img src={good9} alt="" style={{width:14,marginRight:6}} />
+            }
               type = "primary"
               size = "small"
-              className={c.searchBtn}>搜索订单</Button>
-            </div>
+              onClick={()=>get(current)}
+              className={c.searchBtn}>搜索记录</Button>
           </div>
+        </div>
       </div>
-      <Table columns={columns} dataSource={data} size="small" pagination={{
-        showQuickJumper:true,
-        current,
-        pageSize,
-        hideOnSinglePage:true,
-        showLessItems:true,
-        total,
-        onChange
+      <Table
+        columns={columns}
+        dataSource={data}
+        size="small"
+        pagination={{
+          showQuickJumper:true,
+          current,
+          pageSize,
+          showLessItems:true,
+          total,
+          onChange
         }}
       />
     </div>
