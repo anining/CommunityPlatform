772b2fb90f92c5963fae7085a75b0e665f3d3ced
tanjinping <tjp@prismslight.com>
2020-09-29 13:50:01 +0800

    社区订单列表

--- src/views/orderRecordingViews/CommunityOrderView.js
+++ src/views/orderRecordingViews/CommunityOrderView.js
@@ -13,6 +13,9 @@ import TableHeaderComponent from "../../components/TableHeaderComponent";
 import DropdownComponent from "../../components/DropdownComponent";
 import { transformTime, push, getKey } from "../../utils/util";
 import { communityGoodsOrders } from "../../utils/api"
+import SelectComponent from "../../components/SelectComponent"
+
+let win
 
 function CommunityOrderView () {
   const data = [
@@ -70,23 +73,30 @@ function RTable () {
   const [search_user_account, setSearch_user_account] = useState()
   const [search_goods_name, setSearch_goods_name] = useState()
   const [community_goods_category_id, setCommunity_goods_category_id] = useState()
+  const [community_goods_category_name, setCommunity_goods_category_name] = useState()
   const [status, setStatus] = useState()
+
+  window.localClick = function (type, ids) {
+    setCommunity_goods_category_id(ids.id)
+    setCommunity_goods_category_name(ids.name)
+    win && win.close()
+  }
 
   useEffect(() => {
     get(current)
   }, [])
 
   function dateChange (data, dataString) {
-    setDate(dataString)
+    setDate([new Date(dataString[0]).toISOString(), new Date(dataString[1]).toISOString()])
     setMoment(data)
   }
 
   function get (current) {
     communityGoodsOrders(current, pageSize, id, search_user_account, search_goods_name, community_goods_category_id, status, date[0], date[1]).then(r => {
       if (!r.error) {
-        const { data, total } = r
-        setTotal(total)
-        setData(format(data))
+        // const { data, total } = r
+        // setTotal(total)
+        // setData(format(data))
       }
     })
   }
@@ -94,8 +104,8 @@ function RTable () {
   function format (arr) {
     arr.forEach((item, index) => {
       item.key = index
-      item.time = transformTime(item.created_at)
-      item.refund_status = item.refund_status || '-'
+      // item.time = transformTime(item.created_at)
+      // item.refund_status = item.refund_status || '-'
     })
     return arr
   }
@@ -128,28 +138,65 @@ function RTable () {
     // setVisible(localVisible)
   }
 
-  const obj = {
-    pending: {
-      color: "#FF8D30",
-      text: '待处理',
+  function click () {
+    win = window.open("/select-good-category", "_blank", "left=390,top=145,width=1200,height=700")
+  }
+
+  const rowSelection = {
+    onChange: (selectedRowKeys, rows) => {
+      setSelectRows(selectedRowKeys)
     },
-    processing: {
-      color: "#2C68FF",
-      text: '进行中',
-    },
-    completed: {
-      color: "#52C41A",
-      text: '已完成',
-    },
-    // {
-    //   color: "#FF8D30",
-    //   text: '异常',
-    // },
-    closed: {
-      color: "#FF5730",
-      text: '申请退款',
+    selectedRowKeys: selectedRows
+  };
+
+  function onChange (page, pageSize) {
+    setCurrent(page)
+    get(page)
+  }
+
+  function submit (key) {
+    switch (key) {
+      case "delete":
+        message.success('批量删除操作');
+        break
+      default:
+        ;
     }
   }
+
+  function reset () {
+    setId(undefined)
+    setSearch_user_account(undefined)
+    setSearch_goods_name(undefined)
+    setCommunity_goods_category_id(undefined)
+    setCommunity_goods_category_name(undefined)
+    setStatus(undefined)
+    setDate([])
+    setMoment()
+  }
+
+  // const obj = {
+  //   pending: {
+  //     color: "#FF8D30",
+  //     text: '待处理',
+  //   },
+  //   processing: {
+  //     color: "#2C68FF",
+  //     text: '进行中',
+  //   },
+  //   completed: {
+  //     color: "#52C41A",
+  //     text: '已完成',
+  //   },
+  //   // {
+  //   //   color: "#FF8D30",
+  //   //   text: '异常',
+  //   // },
+  //   closed: {
+  //     color: "#FF5730",
+  //     text: '申请退款',
+  //   }
+  // }
   const columns = [
     {
       title: '订单编号',
@@ -205,10 +252,10 @@ function RTable () {
       title: '订单状态',
       align: 'center',
       dataIndex: 'status',
-      render: (text, record, index) => {
-        const { text: t, color } = getKey(text, obj)
-        return <div style={{color}}>{t}</div>
-      }
+      // render: (text, record, index) => {
+      //   const { text: t, color } = getKey(text, obj)
+      //   return <div style={{color}}>{t}</div>
+      // }
   },
     {
       title: '下单时间',
@@ -218,100 +265,74 @@ function RTable () {
     {
       title: '订单历程',
       align: 'center',
-      render: (text, record, index) => <div onClick={()=>push('/main/editCommunityOrder',record)} className={c.clickText}>点击查看</div>
+      // render: (text, record, index) => <div onClick={()=>push('/main/editCommunityOrder',record)} className={c.clickText}>点击查看</div>
     },
     {
       title: '操作',
       align: 'center',
-      render: (text, record, index) => (
-        <Space size="small">
-            <Popconfirm icon={<img src="" alt="" style={{width:0,height:0}}/>
-              }
-              placement = "leftTop"
-              title = {
-                  () => {
-                    // return (
-                    //   // {/* <div style={styles.view}> */}
-                    //   // {/*   <div style={styles.header}> */}
-                    //   // {/*     <img src={good41} alt="" style={styles.icon}/> */}
-                    //   // {/*     <div>请输入需要退款的数量</div> */}
-                    //   // {/*   </div> */}
-                    //   // {/*   <Input style={styles.input} placeholder="请在这里输入退款数量"/> */}
-                    //   // {/*   <div style={styles.tips}>全部退款</div> */}
-                    //   // {/*   <div style={styles.footer}> */}
-                    //   // {/*     <Button size="small" style={styles.cancelBtn}>取消</Button> */}
-                    //   // {/*     <Button size="small" type="primary" style={styles.okBtn}>确定</Button> */}
-                    //   // {/*   </div> */}
-                    //   // {/* </div> */}
-                    // )
-                  }
-                } >
-              <div style={{color:'#FF4D4F',cursor:'wait',textDecorationColor:"#ff4d4f"}} className={c.clickText}>退款</div>
-            </Popconfirm>
-            <div style={{height:14,width:1,background:'#D8D8D8'}}></div>
-            <Popconfirm icon={<img src="" alt="" style={{width:0,height:0}}/>
-              }
-              placement = "leftTop"
-              title = {
-                  () => {
-                    // return (
-                    //   <div style={styles.view}>
-                    //     <div style={styles.header}>
-                    //       <img src={good41} alt="" style={styles.icon}/>
-                    //       <div>请为此订单输入备注信息</div>
-                    //     </div>
-                    //     <Input style={styles.input} placeholder="请在这里输入备注信息"/>
-                    //     <div style={styles.footer}>
-                    //       <Button size="small" style={styles.cancelBtn}>取消</Button>
-                    //       <Button size="small" type="primary" style={styles.okBtn}>确定</Button>
-                    //     </div>
-                    //   </div>
-                    // )
-                  }
-                } >
-              <div style={{cursor:'wait'}} className={c.clickText}>添加备注</div>
-            </Popconfirm>
-        </Space>
-      )
+      // render: (text, record, index) => (
+      //   <Space size="small">
+      //       <Popconfirm icon={<img src="" alt="" style={{width:0,height:0}}/>
+      //         }
+      //         placement = "leftTop"
+      //         title = {
+      //             () => {
+      //               // return (
+      //               //   // {/* <div style={styles.view}> */}
+      //               //   // {/*   <div style={styles.header}> */}
+      //               //   // {/*     <img src={good41} alt="" style={styles.icon}/> */}
+      //               //   // {/*     <div>请输入需要退款的数量</div> */}
+      //               //   // {/*   </div> */}
+      //               //   // {/*   <Input style={styles.input} placeholder="请在这里输入退款数量"/> */}
+      //               //   // {/*   <div style={styles.tips}>全部退款</div> */}
+      //               //   // {/*   <div style={styles.footer}> */}
+      //               //   // {/*     <Button size="small" style={styles.cancelBtn}>取消</Button> */}
+      //               //   // {/*     <Button size="small" type="primary" style={styles.okBtn}>确定</Button> */}
+      //               //   // {/*   </div> */}
+      //               //   // {/* </div> */}
+      //               // )
+      //             }
+      //           } >
+      //         <div style={{color:'#FF4D4F',cursor:'wait',textDecorationColor:"#ff4d4f"}} className={c.clickText}>退款</div>
+      //       </Popconfirm>
+      //       <div style={{height:14,width:1,background:'#D8D8D8'}}></div>
+      //       <Popconfirm icon={<img src="" alt="" style={{width:0,height:0}}/>
+      //         }
+      //         placement = "leftTop"
+      //         title = {
+      //             () => {
+      //               // return (
+      //               //   <div style={styles.view}>
+      //               //     <div style={styles.header}>
+      //               //       <img src={good41} alt="" style={styles.icon}/>
+      //               //       <div>请为此订单输入备注信息</div>
+      //               //     </div>
+      //               //     <Input style={styles.input} placeholder="请在这里输入备注信息"/>
+      //               //     <div style={styles.footer}>
+      //               //       <Button size="small" style={styles.cancelBtn}>取消</Button>
+      //               //       <Button size="small" type="primary" style={styles.okBtn}>确定</Button>
+      //               //     </div>
+      //               //   </div>
+      //               // )
+      //             }
+      //           } >
+      //         <div style={{cursor:'wait'}} className={c.clickText}>添加备注</div>
+      //       </Popconfirm>
+      //   </Space>
+      // )
     }
   ];
-
-  const rowSelection = {
-    onChange: (selectedRowKeys, rows) => {
-      setSelectRows(selectedRowKeys)
-    }
-  };
-
-  function onChange (page, pageSize) {
-    setCurrent(page)
-    get(page)
-  }
-
-  function submit (key) {
-    switch (key) {
-      case "delete":
-        message.success('批量删除操作');
-        break
-      default:
-        ;
-    }
-  }
-
-  function reset () {
-
-  }
 
   return (
     <div className={c.main}>
       <div className={c.searchView}>
         <div className={c.search}>
           <div className={c.searchL}>
-            <Input onPressEnter={()=>get(current)} placeholder="请输入订单编号" size="small" className={c.searchInput}/>
-            <Input onPressEnter={()=>get(current)} placeholder="请输入商品名称" size="small" className={c.searchInput}/>
-            <Input onPressEnter={()=>get(current)} placeholder="请输入下单账号" size="small" className={c.searchInput}/>
-            <DropdownComponent keys={[]} placeholder="请选择商品分类" style={{width:186}}/>
-            <DropdownComponent keys={[]} placeholder="请选择订单状态" style={{width:186}}/>
-            <DropdownComponent keys={[]} placeholder="请选择商品名称" style={{width:186}}/>
+            <Input value={id} onChange={e=>setId(e.target.value)} onPressEnter={()=>get(current)} placeholder="请输入订单编号" size="small" className={c.searchInput}/>
+            <Input onPressEnter={()=>get(current)} value={search_user_account} onChange={e=>setSearch_user_account(e.target.value)} placeholder="请输入用户账户" size="small" className={c.searchInput}/>
+            <Input onPressEnter={()=>get(current)} placeholder="请输入商品名称" value={search_goods_name} onChange={e=>setSearch_goods_name(e.target.value)} size="small" className={c.searchInput}/>
+            <DropdownComponent action={status} setAction={setStatus} keys={[{"name":"待处理",key:"pending"},{"name":"进行中",key:"processing"},{"name":"已完成",key:"completed"},{"name":"已关闭",key:"closed"}]} placeholder="请选择订单状态" style={{width:186}}/>
+            <SelectComponent placeholder="请选择商品分类" id={community_goods_category_id} name={community_goods_category_name} click={click}/>
             <DatePicker.RangePicker
               format="YYYY-MM-DD"
               onChange={dateChange}
